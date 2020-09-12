507433a479f8db82e28a7232c77decda2204b8da
Hendobox <henryonyebuchi53@gmail.com>
2020-09-12 14:10:46 +0100

    added some tests

---
+++ contracts/LeadStake.sol
@@ -0,0 +1,341 @@
+//"SPDX-License-Identifier: UNLICENSED"
+
+pragma solidity ^0.6.0;
+
+import '@openzeppelin/contracts/math/SafeMath.sol';
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract LeadStake is Ownable {
+    //initializing safe computations
+    using SafeMath for uint;
+    
+    //LEAD contract address
+    address public lead;
+    
+    //total amount of staked lead
+    uint public totalStaked;
+    
+    //tax rate for staking in percentage
+    uint public stakingTaxRate;
+    
+    //tax for registration
+    uint public registrationTax;
+    
+    //monthly return of investment in percentage
+    uint8 public monthlyROI;
+    
+    //total amount of LEAD distributed
+    uint public totalDistributed;
+    
+    //tax rate for unstaking in percentage
+    uint public unstakingTaxRate;
+    
+    //minimum stakeable LEAD 
+    uint public minimumStakeValue;
+    
+    //referral allocation from the registration tax
+    uint public referralTaxAllocation;
+    
+    //array of stakeholders' addresses
+    address[] public stakeholders;
+    
+    //mapping of stakeholders' address to number of stakes
+    mapping(address => uint) public stakes;
+    
+    //mapping of stakeholders' address to stake rewards
+    mapping(address => uint) public stakeRewards;
+    
+    //mapping of stakeholders address to number of referrals 
+    mapping(address => uint) public referralCount;
+    
+    //mapping of stakeholders address to referral rewards earned 
+    mapping(address => uint) public referralRewards;
+    
+    //mapping of addresses to verify registered stakers
+    mapping(address => bool) public registered;
+    
+    //Events
+    event OnDistribute(address sender, uint time);
+    event OnWithdrawal(address sender, uint amount);
+    event OnStake(address sender, uint amount, uint tax);
+    event OnUnstake(address sender, uint amount, uint tax);
+    event OnDeposit(address sender, uint amount, uint time);
+    event OnRegisterAndStake(address stakeholder, uint amount, uint totalTax , address _referrer);
+    
+    /**
+     * @dev Sets the initial values for lead, stakingTaxRate, unstakingTaxRate, referralTaxAllocation,
+     * and minimumStakeValue.
+     */
+    constructor(
+        address _token,
+        uint8 _stakingTaxRate, 
+        uint8 _unstakingTaxRate,
+        uint8 _monthlyROI,
+        uint _registrationTax,
+        uint _referralTaxAllocation,
+        uint _minimumStakeValue) public {
+            
+        lead = _token;
+        
+        stakingTaxRate = _stakingTaxRate;
+        
+        unstakingTaxRate = _unstakingTaxRate;
+        
+        monthlyROI = _monthlyROI;
+        
+        registrationTax = _registrationTax;
+        
+        referralTaxAllocation = _referralTaxAllocation;
+        
+        minimumStakeValue = _minimumStakeValue;
+    }
+    
+    //exclusive access for registered address
+    modifier onlyRegistered() {
+        
+        require(registered[msg.sender] == true, "Staker must be registered");
+        
+        _;
+    }
+    
+    //exclusive access for unregistered address
+    modifier onlyUnregistered() {
+        
+        require(registered[msg.sender] == false, "Staker is already registered");
+        _;
+    }
+    
+    /**
+     * registers and creates stakes for new stakeholders
+     * 
+     * deducts the registration tax and staking tax
+     * 
+     * calculates refferal bonus from the registration tax and sends it to the _referrer if there is one
+     * 
+     * transfers LEAD from sender's address into the smart contract
+     *
+     * Emits an {OnRegisterAndStake} event..
+     */
+    function registerAndStake(uint _amount, address _referrer) external onlyUnregistered {
+        
+        require(msg.sender != _referrer, "Cannot refer self");
+        
+        require(IERC20(lead).balanceOf(msg.sender) >= _amount, "Must have enough balance to stake");
+        
+        require(IERC20(lead).transferFrom(msg.sender, address(this), _amount), "Stake failed due to failed amount transfer.");
+        
+        require(_amount >= registrationTax.add(minimumStakeValue), "Must send at least enough LEAD to pay registration fee.");
+        
+        uint referralBonus = (registrationTax.mul(referralTaxAllocation)).div(100);
+        
+        uint finalAmount = _amount.sub(registrationTax);
+        
+        uint stakingTax = (stakingTaxRate.mul(finalAmount)).div(100);
+        
+        if(_referrer != address(0x0)) {
+            
+            referralCount[_referrer]++;
+            
+            referralRewards[_referrer] = referralRewards[_referrer].add(referralBonus);
+            
+        } 
+        
+        stakes[msg.sender] = stakes[msg.sender].add(finalAmount).sub(stakingTax);
+        
+        totalStaked = totalStaked.add(finalAmount).sub(stakingTax);
+        
+        registered[msg.sender] = true;
+        
+        stakeholders.push(msg.sender);
+        
+        emit OnRegisterAndStake(msg.sender, _amount, registrationTax.add(stakingTax), _referrer);
+    }
+    
+    /**
+     * creates stakes for already registered stakeholders
+     * 
+     * deducts the staking tax from _amount inputted
+     * 
+     * registers the remainder in the stakes of the sender
+     *
+     * Emits an {OnStake} event
+     */
+    function stake(uint _amount) external onlyRegistered {
+        
+        require(_amount >= minimumStakeValue, "Amount is below minimum stake value.");
+        
+        require(IERC20(lead).balanceOf(msg.sender) >= _amount, "Must have enough balance to stake");
+        
+        require(IERC20(lead).transferFrom(msg.sender, address(this), _amount), "Stake failed due to failed amount transfer.");
+        
+        uint stakingTax = (stakingTaxRate.mul(_amount)).div(100);
+        
+        totalStaked = totalStaked.add(_amount).sub(stakingTax);
+        
+        stakes[msg.sender] = stakes[msg.sender].add(_amount).sub(stakingTax);
+        
+        emit OnStake(msg.sender, _amount, stakingTax);
+    }
+
+    /**
+     * removes '_amount' stakes for already registered stakeholders
+     * 
+     * deducts the unstaking tax from '_amount'
+     * 
+     * transfers the sum of the remainder, stake rewards and referral rewards to the sender 
+     * 
+     * deregisters stakeholder if all the stakes are removed
+     *
+     * Emits an {OnStake} event
+     */
+    function unstake(uint _amount) external onlyRegistered {
+        
+        require(_amount <= stakes[msg.sender], 'Insufficient balance to unstake');
+        
+        uint unstakingTax = (unstakingTaxRate.mul(_amount)).div(100);
+        
+        stakeRewards[msg.sender] = stakeRewards[msg.sender].add(referralRewards[msg.sender]).add(_amount).sub(unstakingTax);
+        
+        IERC20(lead).transfer(msg.sender, stakeRewards[msg.sender]);
+        
+        stakes[msg.sender] = stakes[msg.sender].sub(_amount);
+        
+        referralRewards[msg.sender] = 0;
+    
+        referralCount[msg.sender] = 0;
+        
+        totalStaked = totalStaked.sub(_amount);
+        
+        if(stakes[msg.sender] == 0) {
+            
+            _removeStakeholder(msg.sender);
+        }
+        
+        emit OnUnstake(msg.sender, _amount, unstakingTax);
+    }
+    
+    /**
+     * deregisters _stakeholder and removes address from stakeholders array
+     */
+    function _removeStakeholder(address _stakeholder) internal {
+        
+        registered[msg.sender] = false;
+        
+        (bool isStakeholder, uint i) = _isStakeholder(_stakeholder);
+        
+        if(isStakeholder){
+            
+            stakeholders[i] = stakeholders[stakeholders.length - 1];
+            
+            stakeholders.pop();
+        }
+    }
+    
+     /**
+     * checks if _address is a registered stakeholder
+     * 
+     * returns 'true' and 'id number' if stakeholder and 'false' and '0'  if not
+     */
+    function _isStakeholder(address _address) internal view returns(bool, uint) {
+        
+        for (uint i = 0; i < stakeholders.length; i += 1){
+            
+            if (_address == stakeholders[i]) {
+                
+                return (true, i);
+            }
+        }
+        
+        return (false, 0);
+    }
+    
+     /**
+     * @dev Should be callsed once on a monthly basis
+     * distributes tokens to the stakeholders' rewards ready for withdrawal
+     */
+    function distribute() external onlyOwner() {
+        
+        uint totalClaimable;
+        
+        for(uint i = 0; i < stakeholders.length; i++){
+            
+            uint reward = _calculatePayment(stakeholders[i]);
+            
+            stakeRewards[stakeholders[i]] = stakeRewards[stakeholders[i]].add(reward);
+            
+            totalClaimable = totalClaimable.add(stakes[stakeholders[i]]).add(stakeRewards[stakeholders[i]]).add(referralRewards[stakeholders[i]]);
+        }
+        
+        if (totalClaimable > IERC20(lead).balanceOf(address(this))) _deposit(totalClaimable.sub(IERC20(lead).balanceOf(address(this))));
+       
+        emit OnDistribute(msg.sender, now);
+    }
+
+    
+    function _calculatePayment(address _stakeholder) internal view returns(uint reward){
+        
+        require(IERC20(lead).balanceOf(address(this)) > 0, 'Empty pool');
+        
+        uint totalReward = referralRewards[_stakeholder].add(stakeRewards[_stakeholder]).add(stakes[_stakeholder]);
+        
+        return (totalReward.mul(monthlyROI)).div(100);                                                       //monthly earning
+    }
+    
+    
+    function _deposit(uint _amount) internal {
+        
+        require(IERC20(lead).balanceOf(msg.sender) >= _amount, 'Insufficient LEAD balance in owner wallet');                //owner must have enough LEAD in wallet
+        
+        IERC20(lead).transferFrom(msg.sender, address(this), _amount);
+       
+        emit OnDeposit(msg.sender, _amount, now);
+    }
+    
+    
+    function withdrawEarnings() public onlyRegistered returns(bool success) {
+        
+        require(stakeRewards[msg.sender] > 0 || referralRewards[msg.sender] > 0, 'No reward to withdraw'); 
+        
+        uint totalReward = referralRewards[msg.sender].add(stakeRewards[msg.sender]);
+        
+        IERC20(lead).transfer(msg.sender, totalReward);
+        
+        stakeRewards[msg.sender] = 0;
+        
+        referralRewards[msg.sender] = 0;
+        
+        referralCount[msg.sender] = 0;
+        
+        emit OnWithdrawal(msg.sender, totalReward);
+        
+        return true;
+    }
+    
+    function setStakingTaxRate(uint8 _stakingTaxRate) external onlyOwner {
+        stakingTaxRate = _stakingTaxRate;
+    }
+    
+    function setUnstakingTaxRate(uint8 _unstakingTaxRate) external onlyOwner {
+        unstakingTaxRate = _unstakingTaxRate;
+    }
+    
+    function setMonthlyROI(uint8 _monthlyROI) external onlyOwner {
+        monthlyROI = _monthlyROI;
+    }
+    
+    function setRegistrationTax(uint _registrationTax) external onlyOwner {
+        registrationTax = _registrationTax;
+    }
+    
+    function setReferralTaxAllocation(uint _referralTaxAllocation) external onlyOwner {
+        referralTaxAllocation = _referralTaxAllocation;
+    }
+    
+    function setMinimumStakeValue(uint _minimumStakeValue) external onlyOwner {
+        minimumStakeValue = _minimumStakeValue;
+    }
+
+}
---
+++ test/leadStake.js
@@ -0,0 +1,191 @@
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
+const LeadStake = artifacts.require('LeadStake');
+const ERC20 = artifacts.require('ERC20');
+
+contract('LeadStake', (accounts) => {
+    let leadStake;
+    let erc20;
+    const [stakeholder1, stakeholder2, stakeholder3] = [accounts[1], accounts[2], accounts[3]];
+  
+    before(async () => {
+        erc20 = await ERC20.deployed();
+        leadStake = await LeadStake.deployed();
+        await erc20.transfer(leadStake.address, 10);
+        await erc20.approve(leadStake.address, 100000000);
+        await erc20.transfer(stakeholder1, 10000);
+        await erc20.transfer(stakeholder2, 10000);
+        await erc20.transfer(stakeholder3, 10000);
+    });
+
+    it('Should transfer a balance of 10 tokens to smart contract properly', async () => {
+        const balance = await erc20.balanceOf(leadStake.address);
+        assert.equal(balance, 10);
+    });
+
+    it('Should NOT create a stake without registration', async () => {
+        await expectRevert(
+            leadStake.stake(2000, {from: stakeholder1}),
+            "Staker must be registered"
+        );
+    });
+
+    it('Should register a stakeholder properly', async () => {
+        await erc20.approve(leadStake.address, 1200, {from: stakeholder1});
+        await leadStake.registerAndStake(1200, stakeholder2, {from: stakeholder1});
+        await erc20.approve(leadStake.address, 2000, {from: stakeholder2});
+        await leadStake.registerAndStake(2000, stakeholder3, {from: stakeholder2});
+        const status1 = await leadStake.registered(stakeholder1);
+        const status2 = await leadStake.registered(stakeholder2);
+        const referralCount2 = await leadStake.referralCount(stakeholder2);
+        const referralCount3 = await leadStake.referralCount(stakeholder3);
+        const referralBonus2 = await leadStake.referralRewards(stakeholder2);
+        const referralBonus3 = await leadStake.referralRewards(stakeholder3);
+        const stakes1 = await leadStake.stakes(stakeholder1);
+        const stakes2 = await leadStake.stakes(stakeholder2);
+        const totalStaked = await leadStake.totalStaked();
+        const stakeholders = await leadStake.stakeholders;
+
+        for(i = 0; i < stakeholders.length; i+1) {
+            let isStakeholder = false;
+            if(stakeholders[i] = stakeholder1 && stakeholder2) {
+                isStakeholder = true;
+            }
+            assert.equal(isStakeholder, true);
+        }
+        assert.equal(status1, true);
+        assert.equal(status2, true);
+        assert.equal(referralCount2.toNumber(), 1);
+        assert.equal(referralCount3.toNumber(), 1);
+        assert.equal(referralBonus2.toNumber(), 100);
+        assert.equal(referralBonus3.toNumber(), 100);
+        assert.equal(stakes1.toNumber(), 980);
+        assert.equal(stakes2.toNumber(), 1764);
+        assert.equal(totalStaked.toNumber(), 2744);
+        
+    });
+    
+    it('Should NOT create registration twice', async () => {
+        await expectRevert(
+            leadStake.registerAndStake(2000, stakeholder3, {from: stakeholder1}),
+            "Staker is already registered"
+        );
+
+        await expectRevert(
+            leadStake.registerAndStake(2000, stakeholder3, {from: stakeholder2}),
+            "Staker is already registered"
+        );
+    });
+
+    it('Should NOT create a stake if amount is below the minimum staking value', async () => {
+        await expectRevert(
+            leadStake.stake(200, {from: stakeholder1}),
+            "Amount is below minimum stake value."
+        );
+    });
+
+    it('Should NOT create a stake if amount is hgher than stakeholder LEAD balance', async () => {
+        await expectRevert(
+            leadStake.stake(20000, {from: stakeholder1}),
+            "Must have enough balance to stake"
+        );
+    });
+
+    it('Should create a stake properly', async () => {
+        await erc20.approve(leadStake.address, 1000, {from: stakeholder1});
+        await leadStake.stake(1000, {from: stakeholder1});
+        const stakes = await leadStake.stakes(stakeholder1);
+        const totalStaked = await leadStake.totalStaked();
+        assert.equal(stakes.toNumber(), 1960);
+        assert.equal(totalStaked.toNumber(), 3724);
+    });
+
+    it('Should NOT distribute rewards if non-owner', async () => {
+        await expectRevert(
+            leadStake.distribute({from: stakeholder2}),
+            "Ownable: caller is not the owner"
+        );
+    });
+
+    it('Should distribute rewards properly', async () => {
+        await leadStake.distribute();
+        const stakeRewardsForStakeholder1 = await leadStake.stakeRewards(stakeholder1);
+        const stakeRewardsForStakeholder2 = await leadStake.stakeRewards(stakeholder2);
+        const stakeRewardsForStakeholder3 = await leadStake.stakeRewards(stakeholder3);
+        assert.equal(stakeRewardsForStakeholder1.toNumber(), 588);
+        assert.equal(stakeRewardsForStakeholder2.toNumber(), 559);
+        assert.equal(stakeRewardsForStakeholder3.toNumber(), 0);    //Should have been 30 instead of 0. But since stakehoder3 isn't registerred, funds wouldn't be disbursed
+    });
+
+    it('Should NOT withdraw for non-registered users', async () => {
+        await expectRevert(
+        leadStake.withdrawEarnings({from: stakeholder3}),
+            "Staker must be registered"
+        );
+    });
+
+    it('Should withdraw properly for registered users', async () => {
+        await leadStake.withdrawEarnings({from: stakeholder2});
+        const stakeRewards = await leadStake.stakeRewards(stakeholder2);
+        const referralRewards = await leadStake.referralRewards(stakeholder2);
+        const referralCount = await leadStake.referralCount(stakeholder2);
+        assert.equal(stakeRewards.toNumber(), 0);
+        assert.equal(referralRewards.toNumber(), 0);
+        assert.equal(referralCount.toNumber(), 0);
+    });
+
+    it('Should NOT withdraw if zero rewards', async () => {
+        await expectRevert(
+        leadStake.withdrawEarnings({from: stakeholder2}),
+            "No reward to withdraw"
+        );
+    });
+
+    it('Should NOT ustake without registration', async () => {
+        await expectRevert(
+        leadStake.stake(2000, {from: stakeholder3}),
+            "Staker must be registered"
+        );
+    });
+
+    it('Should NOT unstake above stake balance', async () => {
+        await expectRevert(
+        leadStake.unstake(1961, {from: stakeholder1}),
+            "Insufficient balance to unstake"
+        );
+    });
+
+    it('Should unstake properly', async () => {
+        await leadStake.unstake(980, {from: stakeholder1});
+        const stakes = await leadStake.stakes(stakeholder1);
+        const referralRewards = await leadStake.referralRewards(stakeholder1);
+        const referralCount = await leadStake.referralCount(stakeholder1);
+        const totalStaked = await leadStake.totalStaked();
+        assert.equal(stakes.toNumber(), 980);
+        assert.equal(referralRewards.toNumber(), 0);
+        assert.equal(referralCount.toNumber(), 0);
+        assert.equal(totalStaked.toNumber(), 2744);
+        
+    });
+
+    it('Should deregister user who unstakes total stakes', async () => {
+        await leadStake.unstake(980, {from: stakeholder1});
+        const stakes = await leadStake.stakes(stakeholder1);
+        const referralRewards = await leadStake.referralRewards(stakeholder1);
+        const referralCount = await leadStake.referralCount(stakeholder1);
+        const totalStaked = await leadStake.totalStaked();
+        const stakeholders = await leadStake.stakeholders;
+        const status = await leadStake.registered(stakeholder1);
+        for(i = 0; i < stakeholders.length; i+1) {
+            let isStakeholder = true;
+            if(stakeholders[i] !== stakeholder1) {
+                isStakeholder = false;
+            }
+            assert.equal(isStakeholder, false);
+        }
+        assert.equal(status, false);
+        assert.equal(stakes.toNumber(), 0);
+        assert.equal(referralRewards.toNumber(), 0);
+        assert.equal(referralCount.toNumber(), 0);
+        assert.equal(totalStaked.toNumber(), 1764);    
+    });
+}); 
